a94edc60bdb4676a7701f1576415e9ed604a9410
Shylesh <shylesharepelly@gmail.com>
2023-05-20 20:38:14 +0530

    built static app

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -5,6 +5,7 @@
     const {id,name,email}=props.contact;
     return (
         <div className="item">
+            <img className="ui avatar image" src={user} alt="user"></img>
         <div className="content">
             <div className="header">{name}</div>
             <div>{email}</div>  
